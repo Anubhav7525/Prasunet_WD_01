35d759b8f103c2e5e8aca723c64c1fb9a8d4c80d
ANUBHAV DIXIT <139374541+Anubhav7525@users.noreply.github.com>
2024-08-12 19:27:59 +0530

    Prasunet_WD_01

---
+++ script.js
@@ -0,0 +1,11 @@
+// script.js
+
+// Add event listener for scrolling
+window.addEventListener('scroll', function() {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
